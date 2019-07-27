17a604c32ba0c675756138153bd6597aba12e48e
Abner-Corona <corona.abner.git@gmail.com>
2019-07-27 10:03:07 -0500

    Axios y jwt-decode

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuelidate from 'vuelidate'
+axios.defaults.baseURL="https://10.0.0.19:45456/api/"
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios, Vuelidate);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
